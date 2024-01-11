ca14c89ace61d7a4cfc9b016946e9ab6cd357b9f
Erik Berglund <rikexhx@gmail.com>
2024-01-11 18:44:02 +0100

    Add project files.

---
+++ Models/StudentGrade.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sql_labb4.Models;
+
+public partial class StudentGrade
+{
+    public int EnrollmentId { get; set; }
+
+    public int CourseId { get; set; }
+
+    public int StudentId { get; set; }
+
+    public int? Grade { get; set; }
+
+    public string? Teacher { get; set; }
+
+    public DateTime? Published { get; set; }
+
+    public virtual Course Course { get; set; } = null!;
+
+    public virtual Person Student { get; set; } = null!;
+}
